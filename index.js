abe7e7e87b5df4c978afc56b4ac6b76a429c07ba
PremaSS <134881121+PremaSS@users.noreply.github.com>
2023-06-20 22:19:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the image link and insert the modal image - use its "data-src" attribute as a source
+let link = document.getElementsByClassName("image-link");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+
+for (let i = 0; i < link.length; i++) {
+    link[i].onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.getAttribute("data-src");
+        captionText.innerHTML = this.innerHTML;
+        this.classList.add("visible-link");
+    }
+}
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+    let visibleLink = document.querySelector(".visible-link");
+    if (visibleLink) {
+        visibleLink.classList.remove("visible-link");
+    }
+}
